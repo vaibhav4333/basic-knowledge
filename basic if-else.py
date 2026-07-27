f47411296aff72cd055f2cca15a1a724d470d85c
vaibhav kumar <vaibhavkumar4333@gmail.com>
2026-07-27 20:34:43 +0530

    basic if-else problem

---
+++ basic if-else.py
@@ -0,0 +1,21 @@
+age = int(input("enter your age:"))
+if age>=18:
+    print("you are eligible for credit card")
+else:
+    print("you are not eligible for credit card") 
+
+
+
+name = input("enter your name:")
+if name == "":
+   print("you did not enter your name")
+else:
+  print(f"hello{name}")
+
+for_sale =True 
+if for_sale==True:
+    print("this item is for sale")
+else:
+    print("this item is not for sale")
+
+    
