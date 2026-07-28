1ed48279e03a32e8c0ee02247ef45a77ab0710d2
vaibhav kumar <vaibhavkumar4333@gmail.com>
2026-07-28 20:26:48 +0530

    Add files via upload
    adding key and values in a dictionary by taking user input

---
+++ making-dict.py
@@ -0,0 +1,18 @@
+d={}
+name=input("enter your name:")
+language=input("enter your language:")
+d.update({name:language})
+
+name=input("enter your name:")
+language=input("enter your language:")
+d.update({name:language})
+
+name=input("enter your name:")
+language=input("enter your language:")
+d.update({name:language})
+
+name=input("enter your name:")
+language=input("enter your language:")
+d.update({name:language})
+
+print("the dictionary is:",d)
