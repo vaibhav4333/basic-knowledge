b0073f5e19e3f96f4c088e473fb6c9925f995a25
vaibhav kumar <vaibhavkumar4333@gmail.com>
2026-07-31 20:21:48 +0530

    Add files via upload

---
+++ for-loop.py
@@ -0,0 +1,15 @@
+#name = input("Enter your name: ")
+
+#for x in reversed(range(1, 8)):
+ #   print(x)
+
+#print("hello programmer")
+
+
+
+for i in range(1,21):
+
+    if i == 10:
+        continue
+    else:
+        print(i)
